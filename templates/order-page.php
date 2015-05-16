3d4a59c32de44b7c9119e1f37328c49fa554af63
nakaikaz <nakaikaz@gmail.com>
2015-05-16 12:04:53 +0900

    modified a bit order page.

--- templates/order-page.php
+++ templates/order-page.php
@@ -19,53 +19,75 @@ $no_header_transition = true;
           </ul>
           <!-- Tab panes -->
           <div class="tab-content">
+            <!-- 電話によるご注文 -->
             <div role="tabpanel" class="tab-pane active" id="byTel">
               <ul class="media-list order-step">
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="img/step-01.gif" alt="..."></div>
-                  <div class="media-body">ホームページのお問い合わせ又はお電話にてお問い合わせください。</div>
+                  <div class="media-left"><img class="media-object" src="img/step-01.gif" alt="01"></div>
+                  <div class="media-body"><a href="/products">販売商品</a>をご確認ください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="img/step-02.gif" alt="..."></div>
-                  <div class="media-body">ご注文確認後、デザインの内容など、詳細をご相談させていただきます。</div>
+                  <div class="media-left"><img class="media-object" src="img/step-02.gif" alt="01"></div>
+                  <div class="media-body">購入商品をお選びください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
-                  <div class="media-body">デザイン案をメール又はＦＡＸにてお送りいたします。</div>
+                  <div class="media-left"><img class="media-object" src="img/step-03.gif" alt="03"></div>
+                  <div class="media-body">お電話にて、最寄りの営業所へご連絡ください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
-                  <div class="media-body">デザイン決定後、制作に入らせていただきます。</div>
+                  <div class="media-left"><img class="media-object" src="img/step-04.gif" alt="04"></div>
+                  <div class="media-body">ご注文。</div>
                 </li>
               </ul>
             </div>
+            <!-- FAXによるご注文 -->
             <div role="tabpanel" class="tab-pane" id="byFax">
               <ul class="media-list order-step">
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
+                  <div class="media-left"><img class="media-object" src="img/step-01.gif" alt="01"></div>
                   <div class="media-body">ホームページのお問い合わせ又はお電話にてお問い合わせください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
+                  <div class="media-left"><img class="media-object" src="img/step-02.gif" alt="02"></div>
                   <div class="media-body">ご注文確認後、デザインの内容など、詳細をご相談させていただきます。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
+                  <div class="media-left"><img class="media-object" src="img/step-03.gif" alt="03"></div>
                   <div class="media-body">デザイン案をメール又はＦＡＸにてお送りいたします。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
-                  <div class="media-left"><img class="media-object" src="http://placehold.it/42x42" alt="..."></div>
+                  <div class="media-left"><img class="media-object" src="img/step-04.gif" alt="04"></div>
                   <div class="media-body">デザイン決定後、制作に入らせていただきます。</div>
                 </li>
               </ul>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="contact-list">
+          <table class="table">
+            <tr>
+              <th>営業所</th><th>ＴＥＬ</th><th>ＦＡＸ</th>
+            </tr>
+            <tr>
+              <td>八戸営業所</td><td>0178-73-2121</td><td>0178-73-2122</td>
+            </tr>
+            <tr>
+              <td>盛岡営業所</td><td>019-647-4649</td><td>019-647-4645</td>
+            </tr>
+            <tr>
+              <td>秋田営業所</td><td>018-816-0067</td><td>018-816-0068</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
