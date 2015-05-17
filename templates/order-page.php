4644fbf4a4dfab316f1d800112602eac7572de9f
nakaikaz <nakaikaz@gmail.com>
2015-05-17 22:13:12 +0900

    modified order page.

--- templates/order-page.php
+++ templates/order-page.php
@@ -48,22 +48,17 @@ $no_header_transition = true;
               <ul class="media-list order-step">
                 <li class="media">
                   <div class="media-left"><img class="media-object" src="img/step-01.gif" alt="01"></div>
-                  <div class="media-body">ホームページのお問い合わせ又はお電話にてお問い合わせください。</div>
+                  <div class="media-body"><a href="#">こちら</a>から、メニュー表をダウンロードしてください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
                   <div class="media-left"><img class="media-object" src="img/step-02.gif" alt="02"></div>
-                  <div class="media-body">ご注文確認後、デザインの内容など、詳細をご相談させていただきます。</div>
+                  <div class="media-body">メニュー表に注文内容をご記入ください。</div>
                 </li>
                 <li class="text-center"><i class="fa fa-caret-down"></i></li>
                 <li class="media">
                   <div class="media-left"><img class="media-object" src="img/step-03.gif" alt="03"></div>
-                  <div class="media-body">デザイン案をメール又はＦＡＸにてお送りいたします。</div>
-                </li>
-                <li class="text-center"><i class="fa fa-caret-down"></i></li>
-                <li class="media">
-                  <div class="media-left"><img class="media-object" src="img/step-04.gif" alt="04"></div>
-                  <div class="media-body">デザイン決定後、制作に入らせていただきます。</div>
+                  <div class="media-body">メニュー表をＦＡＸで送信ください。</div>
                 </li>
               </ul>
             </div>
@@ -99,29 +94,6 @@ $no_header_transition = true;
             確実にご注文の品をお届けするために&nbsp;<strong>３営業日前(土日祝休み)まで</strong>&nbsp;にご注文ください。</p>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-<section id="delivery" class="values sync-image" style="background-image: url('img/bg-aside.jpg');">
-  <div class="container wow fadeIn animated">
-    <div class="row">
-      <div class="col-lg-12">
-        <h2>宅  配</h2>
-        <p>販売車両にオリジナルパンを載せたスタッフが笑顔でお伺いします。</p>
-      </div>
-    </div>
-  </div>
-</section>
-
-<!-- .message-space -->
-<section class="bg-gray message-space">
-  <div class="container wow fadeIn animated">
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <p>「ぱんくらぶ」は青森県・岩手県・秋田県内で、平日週１回、みなさんの職場やご自宅へ宅配販売を行うパン屋です。</p>
-        <p>販売車両にパンをつんで、スタッフがお伺いします。</p>
-        <p class="small">＊&nbsp;宅配地域はですが、一部販売していない地区もありますので各営業所にお電話で確認をお願いします。</p>
       </div>
     </div>
   </div>
