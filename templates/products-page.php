aee72bb0e88232707233539c9ee3f235bc68a45e
nakaikaz <nakaikaz@gmail.com>
2015-06-09 09:35:58 +0900

    modified style of products page.

--- templates/products-page.php
+++ templates/products-page.php
@@ -1,24 +1,55 @@
-<div id="products-list" class="content">
+<section id="products-list">
   <div class="container">
     <div class="row">
       <!-- ロールパン -->
-      <div class="col-md-6 portfolio-item">
+      <div class="col-sm-6 col-md-6 portfolio-item">
         <a href="products/category-one" class="product-group-link">
           <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-one.jpg');"></div>
           <h3 class="link-text">ロールパン<i class="fa fa-angle-right"></i></h3>
         </a>
       </div>
       <!-- デニッシュ -->
-      <div class="col-md-6 portfolio-item">
+      <div class="col-sm-6 col-md-6 portfolio-item">
         <a href="products/category-two" class="product-group-link">
           <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-two.jpg');"></div>
           <h3 class="link-text">デニッシュ<i class="fa fa-angle-right"></i></h3>
         </a>
       </div>
     </div>
-
     <div class="row">
-      <div class="col-md-12 guide-item">
+      <!-- 菓子パン -->
+      <div class="col-sm-6 col-md-6 portfolio-item">
+        <a href="products/category-three" class="product-group-link">
+          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-three.jpg');"></div>
+          <h3 class="link-text">菓子パン<i class="fa fa-angle-right"></i></h3>
+        </a>
+      </div>
+      <!-- 調理パン -->
+      <div class="col-sm-6 col-md-6 portfolio-item">
+        <a href="products/category-four" class="product-group-link">
+          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-four.jpg');"></div>
+          <h3 class="link-text">調理パン<i class="fa fa-angle-right"></i></h3>
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <!-- 洋菓子・和菓子 -->
+      <div class="col-sm-6 col-md-6 portfolio-item">
+        <a href="products/category-five" class="product-group-link">
+          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-five.jpg');"></div>
+          <h3 class="link-text">洋菓子・和菓子<i class="fa fa-angle-right"></i></h3>
+        </a>
+      </div>
+      <!-- その他 -->
+      <div class="col-sm-6 col-md-6 portfolio-item">
+        <a href="products/category-six" class="product-group-link">
+          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-six.jpg');"></div>
+          <h3 class="link-text">その他<i class="fa fa-angle-right"></i></h3>
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 guide-item">
         <a href="<?php echo $base_url; ?>/order">
           <div style="background-image: url('<?php echo $base_url; ?>/img/guide.jpg');" class="guide-cover">
             <h3 class="link-text">ご利用方法<i class="fa fa-angle-right"></i></h3>
@@ -26,40 +57,5 @@
         </a>
       </div>
     </div>
-
-    <div class="row">
-      <!-- 菓子パン -->
-      <div class="col-md-6 portfolio-item">
-        <a href="products/category-three" class="product-group-link">
-          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-three.jpg');"></div>
-          <h3 class="link-text">菓子パン<i class="fa fa-angle-right"></i></h3>
-        </a>
-      </div>
-      <!-- 調理パン -->
-      <div class="col-md-6 portfolio-item">
-        <a href="products/category-four" class="product-group-link">
-          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-four.jpg');"></div>
-          <h3 class="link-text">調理パン<i class="fa fa-angle-right"></i></h3>
-        </a>
-      </div>
-    </div>
-
-    <div class="row">
-      <!-- 洋菓子・和菓子 -->
-      <div class="col-md-6 portfolio-item">
-        <a href="products/category-five" class="product-group-link">
-          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-five.jpg');"></div>
-          <h3 class="link-text">洋菓子・和菓子<i class="fa fa-angle-right"></i></h3>
-        </a>
-      </div>
-      <!-- その他 -->
-      <div class="col-md-6 portfolio-item">
-        <a href="products/category-six" class="product-group-link">
-          <div class="product-group-cover" style="background-image: url('<?php echo $base_url; ?>/img/products-gallery/category-six.jpg');"></div>
-          <h3 class="link-text">その他<i class="fa fa-angle-right"></i></h3>
-        </a>
-      </div>
-    </div>
-
   </div>
-</div>
+</section>
