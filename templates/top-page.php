3cf0bc2cc633b8afb0f41c7292db8a1685fa6c64
nakaikaz <nakaikaz@gmail.com>
2015-07-04 15:21:08 +0900

    replaced a image file on top page.

--- templates/top-page.php
+++ templates/top-page.php
@@ -1,4 +1,4 @@
-<section class="top-cover sync-image content" style="background-image: url('<?php echo $base_url; ?>/img/bg-header.jpg')">
+<section class="top-cover sync-image content" style="background-image: url('<?php echo $base_url; ?>/img/top-cover.jpg')">
   <div class="intro-content">
     <img src="img/logo.png" class="img-responsive img-centered" alt="ぱんらくぶ"/>
   </div>
