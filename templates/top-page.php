7ece714064c2e886264a850a5fdf157b0bc787c4
nakaikaz <nakaikaz@gmail.com>
2015-07-14 21:57:26 +0900

    modified top page.

--- templates/top-page.php
+++ templates/top-page.php
@@ -1,5 +1,5 @@
 <section class="top-cover sync-image content" style="background-image: url('<?php echo $base_url; ?>/img/top-cover.jpg')">
-  <div class="intro-content">
+  <!--<div class="intro-content">
     <img src="img/logo.png" class="img-responsive img-centered" alt="ぱんらくぶ"/>
-  </div>
+  </div>-->
 </section>
